af34b4d88ee3d9781ad4af0d8911fda85d2fa10c
NecmiyeSoylu <necmiyesoyla@gmail.com>
2022-12-04 00:40:06 +0300

    brief for functions

---
+++ fonksiyon.py
@@ -0,0 +1,8 @@
+def hi():
+    print("hi")
+
+hi()
+#todo python un fonk yapısı def der fonk tanımlayacağını belirtirsin fonksiyo adını yazarsın alta geçip fonksiyonun içeriğini yazarsın. 
+#todo yine parametrenin sadece adını yazarız tip yazmaya gerek yok.  return e gelince yine olsada olmasada return tipi yazmaya gerek yok. 
+#todo eğer return döndüreceksen direkt dondür gitsin :))
+#todo birde bu def olayını sadece yukarıda yapabilirsin assağida tanımlayamazsın, java gibi değil.
