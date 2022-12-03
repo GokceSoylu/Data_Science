305f5b77a186fb0539175368dc2594a88e3fb0c8
NecmiyeSoylu <necmiyesoyla@gmail.com>
2022-12-04 00:10:59 +0300

    for each

---
+++ foreach.py
@@ -0,0 +1,9 @@
+#python da foreach yapısı
+a=[1,2,3,4,5]
+for e in a:
+    print(e)
+
+#buraadaki e ismi fark etmez istersen mahmut yaz :)
+# javada for(int a:array) diyorduk ya işte burada değişken tipi belirlemeye gerek yok
+# birde : yerine in yazıyoruz ancak mantık aynı
+#buradki : ifadenin bittiğini belirtmek için var
