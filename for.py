95f1e9fc4d102d5cc86e0f34cd6649d77eb8542e
NecmiyeSoylu <necmiyesoyla@gmail.com>
2022-12-04 00:23:20 +0300

    normal for iteration in list

---
+++ for.py
@@ -0,0 +1,9 @@
+#peki yan normal for nasıl yapılır. hocam bunun için for a ek olarak 
+#range komutunuda kullanmalıyız
+a=[1,2,3,4,5]
+for i in range (0,5,1):
+    print(i*a[i])
+
+#todo for değişken_adı in buraya kadar her zaman aynı eğer foreach olarak kullanacaksan
+#todo dolaşacağı değişkenin adını yazarsın. eğer normal for olarak kullanmak istiuorsan bu kısımdan sonra
+#todo yani for değişken_adi in kısmından sonra range yazarsın sonra (başlangıç,bitiş,adım) değerlerini yazrsın ve tabiki :
