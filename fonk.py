29e411a848869647ac15d3e2c25fb190ba2b3a97
NecmiyeSoylu <necmiyesoyla@gmail.com>
2022-12-04 00:52:50 +0300

    defoult value and overload

---
+++ fonk.py
@@ -0,0 +1,17 @@
+def sum(a=10,b=20):
+    return a+b
+#def sub(a,b,c):
+#    return a-b-c
+#def sub(a,b):
+#    return a-b
+
+print(sum(30))
+#print(sub(10,2,3))
+#print(sub(20,3))
+
+#todo python da defoult değer verebilirsin ancak fonksiyonu overload edemezsin
+# 
+# c++'da defoutl değer verebilir fonksiyonu overload edebilirsin
+# java'da fonksıyonu overload edebilir defoult değer veremezsin
+# python'da defoult değer verebilir overload edmezsin
+# c de hiçbirini yapamazsın
