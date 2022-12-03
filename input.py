fb2c05f6f5f363004c1a4ca73579e9479fccec81
NecmiyeSoylu <necmiyesoyla@gmail.com>
2022-12-03 22:50:20 +0300

    input ve + çeitli kullanımları

---
+++ input.py
@@ -0,0 +1,12 @@
+isim=input("what is your name ")
+by=int(input("what is your birth yaer "))
+presant=int(input("what is year now "))
+print(isim+"'s age is ",presant-by)
+
+print(by+presant)
+
+# print(isim+by+presant) dikkat edelim. yukarıda + işlemini kullanarak " "ve varaible yi bağladım,
+# farklı print de + işaretini toplamak için kullandım sorun yok. ama hem string değişkenşnin hem int değişkenin bulunduğu
+# bulunduğu bir print de yazınca hata aldık
+# ez cümle eğer tipler belliyse toplama için kullanabilirsin. " "olduğu yerde println mantığı ilede kullanbilirsin
+# ancak farklı tiplerin olduğu karılıklık olabilecek yerde yazrsan hata alırsın :))
