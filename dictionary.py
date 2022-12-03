6203fae62ce0d3443d080a31aa97c4794d8db1d3
NecmiyeSoylu <necmiyesoyla@gmail.com>
2022-12-03 22:40:28 +0300

    dict

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+a={"east":0,"west":1,"north":3,"south":4}
+print(a["east"])
+print(a["north"])
+a["west"]=10
+print(a["west"])
+#hocam burada anahtarlarımız string ifadelerimiz, tuttuğu değerler ise yanına yazdığımız değerler oluyor.
+#c ve java daki enum'a ile karıştırmayalım. bu sabit değil bir defa :))
+#adı üztünd bu bir sö<lük bunun karşılığı bu şeklinde belirtiyoruz okadar :))
