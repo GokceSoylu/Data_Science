ab278eb844a07ef9160b38aff869675d100ab2a4
NecmiyeSoylu <necmiyesoyla@gmail.com>
2022-12-04 12:51:35 +0300

    print and +

---
+++ print2.py
@@ -0,0 +1,12 @@
+a="hammal"
+print(len(a))
+print(a[0])
+print(a[5]+"="+a[len(a)-1])# o değilde + yı da kabul etti :))
+print(a[5],"=",a[len(a)-1])
+
+#ama
+#! print("10"+2)
+#burada hata verir çünkü bu toplanabilir bir değer karışıklığa sebep olur
+#+ kullanmak için aynı tip olmalılar
+a=10
+print(a+2)
