39aedf8f90c4b427e9c85687428d9fd4ee469932
NecmiyeSoylu <necmiyesoyla@gmail.com>
2022-12-04 23:21:19 +0300

    creating set, using set()

---
+++ set.py
@@ -0,0 +1,19 @@
+#setler ve dictionary benziyor sırasız, değiştirilebilir, farklı argumanlar tutabilir. 
+#ancak farkı setlerd bir elaman yalnızca  bir defa bulunabilir eğer birden aynı eleman koysanmda set birini kaydeder
+#setler fonksiyon ile oluşturulu
+
+
+#set=set() boş oluşturamazsın
+
+list1=[1,2,3,3,4,5]
+set1=set(list1)
+print(set1)
+
+
+tuple=1,2,3,4,4,4," "," "
+set2=set(tuple)
+print(set2)
+
+
+set3=set("ali_ata_bak")
+print(set3)
