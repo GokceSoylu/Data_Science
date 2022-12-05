f2cf82eefd9fef47e5a41eefa5574be651960f08
NecmiyeSoylu <necmiyesoyla@gmail.com>
2022-12-05 14:39:21 +0300

    new array initiaize method

--- file_process2.py
+++ file_process2.py
@@ -2,3 +2,9 @@ file=open("student","r")
 for line in file:
     print(line)
 file.close
+
+file=open("student","r")
+lines=[]
+lines=[line_ for line_ in file] # tuhaf değişk ancak boylede atam şekli var
+print(lines[:])
+file.close
