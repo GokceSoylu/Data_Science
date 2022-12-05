659fcf92885e4afb8b1f07708d27defd8ea93854
NecmiyeSoylu <necmiyesoyla@gmail.com>
2022-12-05 23:39:16 +0300

    lambda giriş

---
+++ lamda.py
@@ -0,0 +1,16 @@
+#lamda adamdır. lanbda python'a saygımı arttıran kısımdır. Lammbda şanstır. lambda yaşamdır. :))
+#eğer iyi bir yazlımcı olabilrseniz sizde bir gün lambdaları görebilirsiniz.
+
+#hocam lambda dediğimiz yek satırda def kullanmaksızın yazılan fonksiyondur isteğimiz iiş yaptırabiilr, parematre alabilir, return döndürebilir.
+#ancak şartımız tek satır da olmalı, görelim örneği
+
+def square(number):
+    return number**2
+
+print(square(3))
+square_l=lambda number: number**2
+print(square_l(4))
+
+#!devamı lambda2.py dosyasında
+
+
