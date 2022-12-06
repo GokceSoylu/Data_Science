d46cfd6fb48d046fcafc07cde86094c7fdca94f0
NecmiyeSoylu <necmiyesoyla@gmail.com>
2022-12-06 08:01:25 +0300

    vektor processesing wth filter map and and reduce funtions

--- vektor.py
+++ vektor.py
@@ -13,6 +13,22 @@ print(ab[:])
 
 #numpy ssayesinde * ile vektorel çarpabliriz
 aa=np.array([1,2,3,4,5,6])
-bb=np.arry([1,2,3,4,5,6])
+bb=np.array([1,2,3,4,5,6])
 aabb=aa*bb
 print(aabb[:])
+
+#map
+liste=[1,2,3,4,5]
+for itr in liste:
+    print(itr*10)
+
+list(map(lambda a:a*10,liste))
+
+#filter
+liste=[1,2,3,4,5]
+list(filter(lambda x:x%2==0,liste))
+list(filter(lambda n:n-1==0,liste))
+
+#reduce
+from functools import reduce
+reduce(lambda a,b:a*b,liste)
